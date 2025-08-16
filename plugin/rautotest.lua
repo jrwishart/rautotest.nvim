6cb023d3d5b9b8731fd6b26ed45f05955825aee2
jrwishart <justin.wishart@displayr.com>
2025-08-16 22:09:08 +1000

    Remove telescope

--- plugin/rautotest.lua
+++ plugin/rautotest.lua
@@ -7,16 +7,6 @@ vim.g.rautotest = 1
 local rautotest = require("rautotest")
 
 vim.api.nvim_create_user_command(
-    "RAutotestAddTestLinks",
-    rautotest.add_test_links,
-{})
-
-vim.api.nvim_create_user_command(
-    "RAutotestRemoveTestLinks",
-    rautotest.remove_test_links,
-{})
-
-vim.api.nvim_create_user_command(
     "RAutotestRunPlumber",
     rautotest.run_plumber,
 {})
